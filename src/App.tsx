c4c9cd9dd7f0ab975770b1259e081958883b7b7c
Mario Alvarado <m_alvaradox@hotmail.com>
2025-06-25 13:29:19 -0500

    guia 13

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
 
 function App() {
    return (
       <Grid container spacing={5} justifyContent="center" alignItems="center">
 
          {/* Encabezado */}
-         <Grid size = { {xs:12, md:12} }>Elemento: Encabezado</Grid>
+         <Grid size = { {xs:12, md:12} }>Elemento: Encabezado
+            <HeaderUI/>
+         </Grid>
 
          {/* Alertas */}
-         <Grid>Elemento: Alertas</Grid>
+         <Grid container justifyContent="right" alignItems="center">
+            <AlertUI description="No se preveen lluvias"/>
+         </Grid>
 
          {/* Selector */}
          <Grid size = { {xs:12, md:3} }>Elemento: Selector</Grid>
